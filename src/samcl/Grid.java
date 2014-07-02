8e9b07b3fa1b092d55d8a8e78944ecd19edd6d7d
jolly <a210s02@gmail.com>
2014-07-02 23:23:07 +0800

    add the TODO

--- src/samcl/Grid.java
+++ src/samcl/Grid.java
@@ -316,7 +316,7 @@ public class Grid extends MouseAdapter {
 			this.RPCcount++;
 			for (Result result : results) {
 				// List<Cell> Cells = result.listCells();
-
+				//TODO ******most important!!!!
 				for (Cell cell : result.rawCells()) {
 					str = Bytes.toString(CellUtil.cloneQualifier(cell));
 					str = str.replace("(", "");
