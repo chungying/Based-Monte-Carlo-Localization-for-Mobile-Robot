594afc240df572ab0bf6d8bfd258a40a380221a7
chungying <chungying.li@ntnu.edu.tw>
2018-03-16 22:40:20 +0000

    update OdomtricSensor

--- mcls-util/util/pf/sensor/odom/OdometricSensor.java
+++ mcls-util/util/pf/sensor/odom/OdometricSensor.java
@@ -7,12 +7,12 @@ import util.pf.sensor.Sensor;
 
 public class OdometricSensor extends Sensor{
 	
-	@Parameter(names = {"-ocov","--odomcovariance"}, 
+	@Parameter(names = {"-ocov","--odomCovariance"}, 
 			description = "covariance of odometric sensor", 
 			required = false, converter = FloatConverter.class, arity = 1)
 	public float covariance=0.001f;//for simulation, default value is very accurate
 	
-	@Parameter(names = {"-ost","--odomscantime"}, 
+	@Parameter(names = {"-ost","--odomScanTime"}, 
 			description = "the time between two sequensive data", 
 			required = false, converter = FloatConverter.class, arity = 1)
 	public float scanTime=10;//update frequency, unit in ms
@@ -24,7 +24,11 @@ public class OdometricSensor extends Sensor{
 	@Override
 	public void setupSensor(Sensor sensor) throws Exception {
 		super.setupSensor(sensor);
-		
+		if(OdometricSensor.class.isAssignableFrom(sensor.getClass())){
+			OdometricSensor src = (OdometricSensor)sensor;
+			this.scanTime = src.scanTime;
+			this.covariance = src.covariance;
+		}
 	}
 
 }
